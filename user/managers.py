28c2b1cd1092ae6805207f20a691f55aec250c49
ErrorInever <errorinever@gmail.com>
2021-01-14 15:22:37 +0000

    rename

--- user/managers.py
+++ user/managers.py
@@ -1,5 +1,5 @@
 from django.contrib.auth.base_user import BaseUserManager
-from django.utils.translation import ugettext_lazy as text_l
+from django.utils.translation import ugettext_lazy as _
 
 
 class CustomUserManager(BaseUserManager):
@@ -12,7 +12,7 @@ class CustomUserManager(BaseUserManager):
 		Create and save a User
 		"""
 		if not email:
-			raise ValueError(text_l('The Email must be set'))
+			raise ValueError(_('The Email must be set'))
 			email = self.normalize_email(email)
 			user = self.model(email=email, **extra_fields)
 			user.set_password(password)
@@ -28,7 +28,7 @@ class CustomUserManager(BaseUserManager):
 	extra_fields.setdefault('is_active', True)
 
 	if extra_fields.get('is_staff') is not True:
-		raise ValueError(text_l('Superuser must have is_staff=True'))
+		raise ValueError(_('Superuser must have is_staff=True'))
 	if extra_fields.get('is_superuser') is not True:
-		raise ValueError(text_l('Superuser must have is_superuser=True'))
+		raise ValueError(_('Superuser must have is_superuser=True'))
 	return self.create_user(email, password, **extra_fields)
