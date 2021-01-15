0ce9f8165a096b1318623133a2f3a1959ff30963
ErrorInever <errorinever@gmail.com>
2021-01-15 14:32:04 +0000

    fix tuple bug

--- socialmini/settings.py
+++ socialmini/settings.py
@@ -20,9 +20,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
-STATICFILES_DIRS = (
-    os.path.join(BASE_DIR, 'static')
-)
+STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
 STATIC_URL = '/static/'
 LOGIN_URL = '/login/'
 
