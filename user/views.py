c98b7763c0cae84654d2df2c560f70ab4c9d89f7
ErrorInever <errorinever@gmail.com>
2021-01-18 21:12:24 +0000

    add filter

--- user/views.py
+++ user/views.py
@@ -1,8 +1,13 @@
 from rest_framework.viewsets import ModelViewSet
 from user.models import CustomUser
 from user.serializers import CustomUserSerializer
+from django_filters.rest_framework import DjangoFilterBackend
+from rest_framework.filters import SearchFilter
 
 
 class CustomUserViewSet(ModelViewSet):
 	queryset = CustomUser.objects.all()
 	serializer_class = CustomUserSerializer
+	filter_backends = [DjangoFilterBackend, SearchFilter]
+	filter_fields = ['first_name']
+	# search_fields = []
