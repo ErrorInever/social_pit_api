f864a670f26e64b5e447e349dd985a7d7b25221d
ErrorInever <errorinever@gmail.com>
2021-01-19 20:37:00 +0000

    add view auth template

--- user/views.py
+++ user/views.py
@@ -3,11 +3,18 @@ from user.models import CustomUser
 from user.serializers import CustomUserSerializer
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import SearchFilter
+from rest_framework.permissions import IsAuthenticated
+from django.shortcuts import render
 
 
 class CustomUserViewSet(ModelViewSet):
 	queryset = CustomUser.objects.all()
 	serializer_class = CustomUserSerializer
 	filter_backends = [DjangoFilterBackend, SearchFilter]
+	permission_classes = [IsAuthenticated]
 	filter_fields = ['first_name']
 	# search_fields = []
+
+
+def auth(request):
+	return render(request, 'oauth.html')
