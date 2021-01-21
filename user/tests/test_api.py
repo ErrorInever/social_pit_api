3729cc6a18c16213b39deec2d57e6b6d034a93ee
ErrorInever <errorinever@gmail.com>
2021-01-21 17:30:27 +0000

    test post api

--- user/tests/test_api.py
+++ user/tests/test_api.py
@@ -2,11 +2,11 @@ from rest_framework import status
 from rest_framework.test import APITestCase
 from django.urls import reverse
 from django.contrib.auth import get_user_model
-from user.serializers import CustomUserSerializer
-
+from user.serializers import CustomUserSerializer, PostSerializer
+from user.models import Post
 
 class CustomUserApiTestCase(APITestCase):
-	def test_get(self):
+	def _get(self):
 		User = get_user_model()
 		user_1 = User.objects.create_user(
 			email='some@mail.ru',
@@ -29,3 +29,36 @@ class CustomUserApiTestCase(APITestCase):
 		serializer_data = CustomUserSerializer([user_1, user_2], many=True).data
 		self.assertEqual(status.HTTP_200_OK, response.status_code)
 		# self.assertEqual(serializer_data, response.data)
+
+
+class PostApiTestCase(APITestCase):
+	def test_get(self):
+		User = get_user_model()
+		user = User.objects.create_user(
+			email='some@user.com', 
+			password='password',
+			first_name='John',
+			last_name='Bath',
+			hometown='Moscow',
+			bio='Some time some text'
+		)
+		post_1 = Post(
+			author=user,
+			content="Its content text 111111",
+			title="title_post_1"
+		)
+		post_2 = Post(
+			author=user,
+			content="Its content text 22222",
+			title="title_post_2"
+		)
+		post_3 = Post(
+			author=user,
+			content="Its content text 33333",
+			title="title_post_3"
+		)
+		serializer_data = PostSerializer([post_1, post_2, post_3], many=True).data
+		url = reverse('post-list')
+		response = self.client.get(url)
+		self.assertEqual(status.HTTP_200_OK, response.status_code)
+		self.assertEqual(serializer_data, response.data)
