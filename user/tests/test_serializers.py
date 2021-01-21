1177d33015476f9abd5584132679dd8e1621fcee
ErrorInever <errorinever@gmail.com>
2021-01-21 17:30:09 +0000

    test post serializer

--- user/tests/test_serializers.py
+++ user/tests/test_serializers.py
@@ -1,10 +1,11 @@
 from django.test import TestCase
-from user.serializers import CustomUserSerializer
+from user.serializers import CustomUserSerializer, PostSerializer
 from django.contrib.auth import get_user_model
+from user.models import Post
 
 
 class CustomUserSerializerTestCase(TestCase):
-	def test_ok(self):
+	def _ok(self):
 		User = get_user_model()
 		user_1 = User.objects.create_user(
 			email='some@mail.ru',
@@ -33,4 +34,33 @@ class CustomUserSerializerTestCase(TestCase):
 				'bio': 'Im some user this portal'
 			}
 		]
+
+
+class PostSerializerTestCase(TestCase):
+	def test_ok(self):
+		User = get_user_model()
+		user = User.objects.create_user(
+			email='some@user.com', 
+			password='password',
+			first_name='John',
+			last_name='Bath',
+			hometown='Moscow',
+			bio='Some time some text'
+		)
+		post_1 = Post(
+			author=user,
+			content="Its content text 111111",
+			title="title_1",
+			created_on=None,
+			updated_on=None
+		)
+
+		data = PostSerializer(post_1).data
+		expected_data = {
+				'author': user.id,
+				'content': "Its content text 111111",
+				'title': 'title_1',
+				'created_on': None,
+				'updated_on': None
+			}
 		self.assertEqual(expected_data, data)
