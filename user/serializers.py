ea330c7f18276c13a3f52d2d36c06c598fdfcabe
ErrorInever <errorinever@gmail.com>
2021-01-21 17:30:52 +0000

    add post serializer

--- user/serializers.py
+++ user/serializers.py
@@ -1,8 +1,14 @@
 from rest_framework.serializers import ModelSerializer
-from user.models import CustomUser
+from user.models import CustomUser, Post
 
 
 class CustomUserSerializer(ModelSerializer):
 	class Meta:
 		model = CustomUser
 		fields = '__all__'
+
+
+class PostSerializer(ModelSerializer):
+	class Meta:
+		model = Post
+		fields = ['author', 'content', 'title', 'created_on', 'updated_on']
