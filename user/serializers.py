271d9e7a0ff19fdfcbcb54deb647ed8e7120530a
ErrorInever <errorinever@gmail.com>
2021-01-23 23:11:43 +0000

    add postrelation serializer

--- user/serializers.py
+++ user/serializers.py
@@ -1,5 +1,5 @@
 from rest_framework.serializers import ModelSerializer
-from user.models import CustomUser, Post
+from user.models import CustomUser, Post, UserPostRelation
 
 
 class CustomUserSerializer(ModelSerializer):
@@ -12,3 +12,9 @@ class PostSerializer(ModelSerializer):
 	class Meta:
 		model = Post
 		fields = ['id', 'author', 'content', 'title', 'created_on', 'updated_on']
+
+
+class UserPostRelationSerializer(ModelSerializer):
+	class Meta:
+		model = UserPostRelation
+		fields = ('post', 'like', 'rate')
