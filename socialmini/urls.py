92f290a42ba398d76e1340e3e787f0e1584ffb13
ErrorInever <errorinever@gmail.com>
2021-01-23 23:10:11 +0000

    add postrelation url

--- socialmini/urls.py
+++ socialmini/urls.py
@@ -17,18 +17,19 @@ from django.urls import path, include
 from django.conf.urls import url
 from django.contrib import admin
 from rest_framework.routers import SimpleRouter
-from user.views import CustomUserViewSet, auth, index, PostViewSet
+from user.views import CustomUserViewSet, auth, index, PostViewSet, UserPostRelationView
 
 
 router = SimpleRouter()
 router.register(r'api/users', CustomUserViewSet, basename='users')
 router.register(r'api/post', PostViewSet, basename='post')
+router.register(r'api/post_relation', UserPostRelationView, basename='post_relation')
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     url('', include('social_django.urls', namespace='social')),
     path('auth/', auth),
-    path('', index)
+    #path('', index)
 ]
 
 urlpatterns += router.urls
