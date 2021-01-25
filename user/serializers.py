ab526ae7a7970c608cb89319d3926b066bf6de16
ErrorInever <errorinever@gmail.com>
2021-01-25 23:51:43 +0000

    optimize SQL queryes, add related func

--- user/serializers.py
+++ user/serializers.py
@@ -12,11 +12,12 @@ class CustomUserSerializer(ModelSerializer):
 class PostSerializer(ModelSerializer):
 	annotated_likes = serializers.IntegerField(read_only=True)
 	rating = serializers.DecimalField(max_digits=3, decimal_places=2, read_only=True)
-
+	owner_name = serializers.CharField(source='author.first_name', read_only=True)
 
 	class Meta:
 		model = Post
-		fields = ['id', 'author', 'content', 'title', 'created_on', 'updated_on', 'annotated_likes', 'rating']
+		fields = ['id', 'author', 'content', 'title', 'created_on', 'updated_on', 
+		'annotated_likes', 'rating', 'owner_name']
 
 
 
