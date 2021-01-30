abfbfde001772a6084e4242ce4b57df27529b03e
ErrorInever <errorinever@gmail.com>
2021-01-30 23:04:20 +0000

    add route friendlist

--- socialmini/urls.py
+++ socialmini/urls.py
@@ -4,13 +4,15 @@ from django.conf.urls import url
 from django.contrib import admin
 from rest_framework.routers import SimpleRouter
 from user.views import CustomUserViewSet, auth, index, PostViewSet, UserPostRelationView
-
+from friend.views import FriendListViewSet
 
 
 router = SimpleRouter()
 router.register(r'api/users', CustomUserViewSet, basename='users')
 router.register(r'api/post', PostViewSet, basename='post')
 router.register(r'api/post_relation', UserPostRelationView, basename='post_relation')
+router.register(r'api/friend_list', FriendListViewSet, basename='friend_list')
+
 
 urlpatterns = [
     path('admin/', admin.site.urls),
